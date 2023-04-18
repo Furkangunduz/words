04839e93fc301fe6bd74e6f6999b786bde14ab06
Furkan Gündüz <furkan.gundz00@gmail.com>
2023-04-18 23:42:01 +0300

    Initial.

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Test from "./apps/Test";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App() {
+  return <Test></Test>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ apps/Test.js
@@ -0,0 +1,34 @@
+import { View, Text, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button title='Go to Details' onPress={() => navigation.push("Details")} />
+    </View>
+  );
+}
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button title='Go to Details' onPress={() => navigation.push("Home")} />
+    </View>
+  );
+}
+const Stack = createNativeStackNavigator();
+
+export default function Test() {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeScreen} />
+          <Stack.Screen name='Details' component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
