558a203d822217e3f1b2c086f8148aec6c14ed88
unknown <www.yunuscinar4141@gmail.com>
2023-04-18 23:56:56 +0300

    native web dependencies added

--- apps/Test.js
+++ apps/Test.js
@@ -14,7 +14,7 @@ function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Details Screen</Text>
-      <Button title='Go to Details' onPress={() => navigation.push("Home")} />
+      <Button title='Go to Home' onPress={() => navigation.push("Home")} />
     </View>
   );
 }
