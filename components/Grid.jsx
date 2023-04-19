59d7be74f548f4286a648981cca7b7a7d3c0ff4c
Furkan Gündüz <furkan.gundz00@gmail.com>
2023-04-19 21:50:20 +0300

    Flat list Height doğru degere getirildi.

--- components/Grid.jsx
+++ components/Grid.jsx
@@ -1,12 +1,10 @@
 // import React from "react";
 import { View, FlatList } from "react-native";
 import GridCell from "./GridCell";
-import { Dimensions } from "react-native";
 
 export default ({ data, onPressItem, styles }) => {
-  const { height } = Dimensions.get("window");
   return (
-    <View style={{ height, marginTop: height / 4 }}>
+    <View style={styles.flatlistContainer}>
       <FlatList
         data={data}
         keyExtractor={(_, index) => index.toString()}
