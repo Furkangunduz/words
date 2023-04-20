5e76c85cbb7b988a10d3729452207be1c26e68a1
Furkan Gündüz <furkan.gundz00@gmail.com>
2023-04-20 23:22:13 +0300

    Button image renkleri beyaz yapıldı.

--- components/Controls.jsx
+++ components/Controls.jsx
@@ -20,13 +20,13 @@ export default Controls = ({ styles, choosenText, onPressCancel, onPressSubmit }
       <View style={styles.controlButtons}>
         <TouchableOpacity onPress={() => onPressSubmit()}>
           <View style={[styles.button, styles.green_bg]}>
-            <Image source={require("../assets/check.png")} style={[styles.image]}></Image>
+            <Image source={require("../assets/check.png")} style={[styles.image, { tintColor: "white" }]}></Image>
           </View>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={() => onPressCancel()}>
           <View style={[styles.button, styles.red_bg]}>
-            <Image source={require("../assets/xmark.png")} style={[styles.image]}></Image>
+            <Image source={require("../assets/xmark.png")} style={[styles.image, { tintColor: "white" }]}></Image>
           </View>
         </TouchableOpacity>
       </View>
