ec7e39c08a968a871a8d2d361a4d85a5113b23fa
Furkan Gündüz <furkan.gundz00@gmail.com>
2023-04-21 00:55:18 +0300

    Düşen butonlara animasyon eklendi.

--- components/GridCell.jsx
+++ components/GridCell.jsx
@@ -1,10 +1,29 @@
-import React from "react";
-import { TouchableOpacity, View, Text } from "react-native";
+import { TouchableOpacity, View, Text, Animated } from "react-native";
+import { useEffect, useRef } from "react";
+
+const fadeAnim = new Animated.Value(0);
 
 export default ({ item, onPressItem, styles = {}, isGameOver }) => {
   const backgroundColor = item.isSelected ? "red" : "white";
   const color = item.isSelected ? "white" : "black";
-  const { letter } = item;
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    if (item.letter) {
+      Animated.timing(fadeAnim, {
+        toValue: 0.9,
+        duration: 750,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 750,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [item.letter]);
 
   return (
     <TouchableOpacity
@@ -14,13 +33,13 @@ export default ({ item, onPressItem, styles = {}, isGameOver }) => {
           backgroundColor,
         },
       ]}
-      disabled={letter == undefined || letter == null || letter == 0 || item.isSelected || isGameOver}
+      disabled={item.letter == undefined || item.letter == null || item.letter == 0 || item.isSelected}
       onPress={() => onPressItem(item)}
     >
-      {letter && (
-        <View>
+      {item.letter && (
+        <Animated.View style={{ opacity: fadeAnim }}>
           <Text style={[styles.letter, { color }]}>{item.letter}</Text>
-        </View>
+        </Animated.View>
       )}
     </TouchableOpacity>
   );
